27c8df0a82c84801c4580e55314d8d26687dbdac
Evan Butler <evanb447@gmail.com>
2019-01-23 09:24:08 -0800

    Reverts to default theme and adds Theme type assertion

---
+++ styles/theme.ts
@@ -0,0 +1,12 @@
+import { DefaultTheme, Theme } from 'react-native-paper'
+/**
+ * Paper UI Theme
+ */
+export const theme: Theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    // primary: 'tomato',
+    // accent: 'yellow',
+  },
+}
