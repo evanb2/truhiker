78d87ff7dfb121afd3a8af72697a5435753411c8
Evan Butler <evanb447@gmail.com>
2019-01-23 21:43:16 -0800

    Adds and links react-native-google-signin

---
+++ utils/googleSignin.ts
@@ -0,0 +1,26 @@
+import firebase from 'react-native-firebase'
+import { GoogleSignin } from 'react-native-google-signin'
+
+// Calling this function will open Google for login.
+export async function googleLogin() {
+  try {
+    // add any configuration settings here:
+    await GoogleSignin.configure()
+
+    const data = await GoogleSignin.signIn()
+
+    // create a new firebase credential with the token
+    const credential = firebase.auth.GoogleAuthProvider.credential(
+      data.idToken,
+      data.accessToken || undefined
+    )
+    // login with credential
+    const firebaseUserCredential = await firebase
+      .auth()
+      .signInWithCredential(credential)
+
+    console.warn(JSON.stringify(firebaseUserCredential.user.toJSON()))
+  } catch (e) {
+    console.error(e)
+  }
+}
