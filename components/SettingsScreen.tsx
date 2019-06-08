b5cd4aa577185eba644013054742129c73c88b75
Evan Butler <evanb447@gmail.com>
2019-06-08 13:24:37 -0600

    Adds a temporary logout button

--- components/SettingsScreen.tsx
+++ components/SettingsScreen.tsx
@@ -1,17 +1,26 @@
+import firebase from 'firebase'
+import 'firebase/auth'
 import React, { Component } from 'react'
 import { View } from 'react-native'
-import { Text } from 'react-native-paper'
+import { Button } from 'react-native-paper'
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
 import { NavigationScreenProps } from 'react-navigation'
 
 export class SettingsScreen extends Component<NavigationScreenProps> {
   static navigationOptions = {
-    tabBarIcon: () => <FontAwesome5 name={'cog'} solid />,
+    tabBarIcon: () => <FontAwesome5 name={'cog'} solid size={25} />,
   }
+
+  logout = () => {
+    firebase.auth().signOut()
+  }
+
   render() {
     return (
-      <View>
-        <Text>Settings</Text>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Button mode="contained" onPress={this.logout}>
+          Logout
+        </Button>
       </View>
     )
   }
