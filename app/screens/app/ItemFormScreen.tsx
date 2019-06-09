11f1e7218cc5387a1865933704e6acb201a9f496
Evan Butler <evanb447@gmail.com>
2019-06-08 18:41:10 -0600

    Adds fields to ItemFormScreen

--- app/screens/app/ItemFormScreen.tsx
+++ app/screens/app/ItemFormScreen.tsx
@@ -1,17 +1,136 @@
+import { AntDesign, MaterialCommunityIcons } from '@expo/vector-icons'
 import React, { Component } from 'react'
 import { StyleSheet, View } from 'react-native'
-import { Title } from 'react-native-paper'
+import { TouchableOpacity, State } from 'react-native-gesture-handler'
+import { TextInput } from 'react-native-paper'
+import { theme } from 'styles/theme'
 
-export class ItemFormScreen extends Component {
+enum Units {
+  OUNCE = 'oz',
+  POUND = 'lb',
+  GRAM = 'g',
+}
+
+interface State {
+  name: string
+  description: string
+  price: number
+  weight: number
+  units: Units
+  quantity: number
+  photoUrl: string
+  linkUrl: string
+  worn: boolean
+  consumable: boolean
+}
+
+export class ItemFormScreen extends Component<State> {
+  state = {
+    name: '',
+    description: '',
+    price: '',
+    weight: '',
+    linkURL: '',
+    worn: false,
+    consumable: false,
+  }
+
+  toggleWorn = () => {
+    this.setState((state: State) => ({
+      worn: !state.worn,
+    }))
+  }
+
+  toggleConsumable = () => {
+    this.setState((state: State) => ({
+      consumable: !state.consumable,
+    }))
+  }
+
   render() {
+    const {
+      name,
+      description,
+      price,
+      weight,
+      linkURL,
+      worn,
+      consumable,
+    } = this.state
+
     return (
       <View style={_styles.screenContainer}>
-        <Title>Add Item</Title>
+        <TextInput
+          style={_styles.textInputContainer}
+          label="Name"
+          value={name}
+        />
+        <TextInput
+          style={_styles.textInputContainer}
+          label="Description"
+          value={description}
+        />
+        <View style={_styles.textInputRow}>
+          <TextInput
+            style={[_styles.textInputContainer, { flex: 1, marginRight: 4 }]}
+            label="Price"
+            value={price}
+          />
+          <TextInput
+            style={[_styles.textInputContainer, { flex: 1, marginLeft: 4 }]}
+            label="Weight"
+            value={weight}
+          />
+        </View>
+        <TextInput
+          style={_styles.textInputContainer}
+          label="Link"
+          value={linkURL}
+        />
+        <View style={_styles.buttonsRow}>
+          <TouchableOpacity onPress={this.toggleWorn} style={_styles.button}>
+            <AntDesign
+              name="skin"
+              size={35}
+              color={worn ? theme.colors.primary : theme.colors.disabled}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={this.toggleConsumable}
+            style={_styles.button}
+          >
+            <MaterialCommunityIcons
+              name="silverware-variant"
+              size={35}
+              color={consumable ? theme.colors.primary : theme.colors.disabled}
+            />
+          </TouchableOpacity>
+        </View>
       </View>
     )
   }
 }
 
 const _styles = StyleSheet.create({
-  screenContainer: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+  screenContainer: {
+    flex: 1,
+    backgroundColor: 'white',
+    paddingHorizontal: 16,
+    paddingTop: 16,
+  },
+  textInputContainer: { marginVertical: 4 },
+  textInputRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+  },
+  buttonsRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 20,
+  },
+  button: {
+    marginHorizontal: 16,
+  },
 })
