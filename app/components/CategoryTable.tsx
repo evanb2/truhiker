a997ec96f4b3daccad6815b018c4934c9b4181e6
Evan Butler <evanb447@gmail.com>
2019-06-16 16:24:42 -0600

    Adds totalWeight calculation in lbs

--- app/components/CategoryTable.tsx
+++ app/components/CategoryTable.tsx
@@ -1,20 +1,40 @@
+import { SimpleLineIcons } from '@expo/vector-icons'
 import React, { Component } from 'react'
 import { StyleSheet, View } from 'react-native'
+import { TouchableOpacity } from 'react-native-gesture-handler'
 import { Subheading, Surface, Text } from 'react-native-paper'
-import { PackItem } from 'utils/types'
+import { PackItem, WeightUnit } from 'utils/types'
 
 interface Props {
+  categoryName: string
   categoryItems: PackItem[]
+  onAddItems: (category: string) => void
 }
 
 export class CategoryTable extends Component<Props> {
   render() {
-    const { categoryItems } = this.props
-    const category = categoryItems[0].category
+    const { categoryName, categoryItems, onAddItems } = this.props
+
+    const totalWeight = categoryItems
+      .map(item => {
+        const weight = Number(item.weight)
+        if (item.units === WeightUnit.GRAMS) {
+          return weight / 453.592
+        }
+        if (item.units === WeightUnit.KILOGRAMS) {
+          return weight * 2.205
+        }
+        if (item.units === WeightUnit.OUNCES) {
+          return weight / 16
+        }
+        return weight
+      })
+      .reduce((prevVal, current) => prevVal + current)
+      .toFixed(2)
 
     return (
       <Surface style={_styles.surface}>
-        <Subheading>{category}</Subheading>
+        <Subheading style={{ fontWeight: 'bold' }}>{categoryName}</Subheading>
         {categoryItems.map((packItem: PackItem) => (
           <View style={_styles.tableItemRow} key={packItem.uid}>
             <View style={{ flex: 0.5 }}>
@@ -28,6 +48,21 @@ export class CategoryTable extends Component<Props> {
             </View>
           </View>
         ))}
+        <View
+          style={{ flexDirection: 'row', alignItems: 'center', marginTop: 8 }}
+        >
+          <View style={{ flex: 0.2 }}>
+            <TouchableOpacity
+              style={_styles.addItemButton}
+              onPress={() => onAddItems(categoryName)}
+            >
+              <SimpleLineIcons name="plus" size={20} />
+            </TouchableOpacity>
+          </View>
+          <View style={{ flex: 1, alignItems: 'flex-end' }}>
+            <Text style={{ fontWeight: 'bold' }}>{`${totalWeight} lbs`}</Text>
+          </View>
+        </View>
       </Surface>
     )
   }
@@ -46,8 +81,10 @@ const _styles = StyleSheet.create({
     justifyContent: 'space-evenly',
   },
   tableItemRow: {
+    marginVertical: 2,
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-evenly',
   },
+  // addItemButton: { marginTop: 8 },
 })
