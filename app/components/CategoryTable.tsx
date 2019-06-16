ccb06643d8cc438aa00b064d9f4cf779061c0b86
Evan Butler <evanb447@gmail.com>
2019-06-16 17:24:42 -0600

    Starts restyling item in CategoryTable row

--- app/components/CategoryTable.tsx
+++ app/components/CategoryTable.tsx
@@ -3,17 +3,24 @@ import React, { Component } from 'react'
 import { StyleSheet, View } from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 import { Subheading, Surface, Text } from 'react-native-paper'
+import { theme } from 'styles/theme'
 import { PackItem, WeightUnit } from 'utils/types'
 
 interface Props {
   categoryName: string
   categoryItems: PackItem[]
   onAddItems: (category: string) => void
+  onDeleteCategory: (category: string) => void
 }
 
 export class CategoryTable extends Component<Props> {
   render() {
-    const { categoryName, categoryItems, onAddItems } = this.props
+    const {
+      categoryName,
+      categoryItems,
+      onAddItems,
+      onDeleteCategory,
+    } = this.props
 
     const totalWeight = categoryItems
       .map(item => {
@@ -34,13 +41,24 @@ export class CategoryTable extends Component<Props> {
 
     return (
       <Surface style={_styles.surface}>
-        <Subheading style={{ fontWeight: 'bold' }}>{categoryName}</Subheading>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+          }}
+        >
+          <Subheading style={{ fontWeight: 'bold', marginLeft: 2 }}>
+            {categoryName}
+          </Subheading>
+          <TouchableOpacity onPress={() => onAddItems(categoryName)}>
+            <SimpleLineIcons name="plus" size={20} />
+          </TouchableOpacity>
+        </View>
         {categoryItems.map((packItem: PackItem) => (
           <View style={_styles.tableItemRow} key={packItem.uid}>
-            <View style={{ flex: 0.5 }}>
-              <Text>{packItem.name}</Text>
-            </View>
             <View style={{ flex: 1 }}>
+              <Text>{packItem.name}</Text>
               <Text>{packItem.description}</Text>
             </View>
             <View style={{ flex: 0.3, alignItems: 'flex-end' }}>
@@ -51,16 +69,13 @@ export class CategoryTable extends Component<Props> {
         <View
           style={{ flexDirection: 'row', alignItems: 'center', marginTop: 8 }}
         >
-          <View style={{ flex: 0.2 }}>
-            <TouchableOpacity
-              style={_styles.addItemButton}
-              onPress={() => onAddItems(categoryName)}
-            >
-              <SimpleLineIcons name="plus" size={20} />
-            </TouchableOpacity>
-          </View>
+          <TouchableOpacity onPress={() => onDeleteCategory(categoryName)}>
+            <SimpleLineIcons name="trash" size={20} />
+          </TouchableOpacity>
           <View style={{ flex: 1, alignItems: 'flex-end' }}>
-            <Text style={{ fontWeight: 'bold' }}>{`${totalWeight} lbs`}</Text>
+            <Text style={{ fontWeight: 'bold', marginRight: 4 }}>
+              {`${totalWeight} lbs`}
+            </Text>
           </View>
         </View>
       </Surface>
@@ -81,10 +96,12 @@ const _styles = StyleSheet.create({
     justifyContent: 'space-evenly',
   },
   tableItemRow: {
-    marginVertical: 2,
+    marginVertical: 4,
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-evenly',
+    borderRadius: 10,
+    padding: 8,
+    backgroundColor: theme.colors.disabled,
   },
-  // addItemButton: { marginTop: 8 },
 })
