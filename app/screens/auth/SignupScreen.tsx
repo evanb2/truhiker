c9690300bcd83f4fdcabcf28ec9f287be9445064
Evan Butler <evanb447@gmail.com>
2019-06-16 19:28:31 -0600

    Implements rudimentary SignUp screen

---
+++ app/screens/auth/SignupScreen.tsx
@@ -0,0 +1,85 @@
+import firebase from 'firebase'
+// import 'firebase/auth'
+import React, { Component } from 'react'
+import { StyleSheet, View } from 'react-native'
+import { Button, TextInput } from 'react-native-paper'
+import { NavigationScreenProps } from 'react-navigation'
+
+export default class SignupScreen extends Component<NavigationScreenProps> {
+  state = {
+    email: '',
+    password: '',
+    confirmPassword: '',
+  }
+
+  /**
+   * Register new user using email/password.
+   */
+  signUp = (): void => {
+    const { email, password } = this.state
+
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(() => {
+        firebase.auth().signInWithEmailAndPassword(email, password)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+
+  render() {
+    const { email, password, confirmPassword } = this.state
+
+    const disabled =
+      !password || !confirmPassword || password !== confirmPassword
+
+    return (
+      <View style={_styles.screenContainer}>
+        <TextInput
+          autoFocus
+          style={{ width: '100%', marginVertical: 4 }}
+          mode={'outlined'}
+          label={'Email'}
+          value={email}
+          onChangeText={val => this.setState({ email: val })}
+          keyboardType={'email-address'}
+          autoCorrect={false}
+          autoCapitalize={'none'}
+        />
+        <TextInput
+          style={{ width: '100%', marginVertical: 4 }}
+          mode={'outlined'}
+          label={'Password'}
+          value={password}
+          onChangeText={val => this.setState({ password: val })}
+          autoCorrect={false}
+          secureTextEntry={true}
+        />
+        <TextInput
+          style={{ width: '100%', marginVertical: 4 }}
+          mode={'outlined'}
+          label={'Confirm Password'}
+          value={confirmPassword}
+          onChangeText={val => this.setState({ confirmPassword: val })}
+          autoCorrect={false}
+          secureTextEntry={true}
+        />
+        <Button
+          mode="contained"
+          uppercase={false}
+          style={{ marginTop: 8 }}
+          disabled={disabled}
+          onPress={this.signUp}
+        >
+          Signup
+        </Button>
+      </View>
+    )
+  }
+}
+
+const _styles = StyleSheet.create({
+  screenContainer: { flex: 1, padding: 16 },
+})
---
+++ app/screens/auth/navigator.ts
@@ -0,0 +1,30 @@
+import { createStackNavigator } from 'react-navigation'
+import { AuthScreen } from 'screens/auth/AuthScreen'
+import SignupScreen from 'screens/auth/SignupScreen'
+import { Routes } from 'screens/routes'
+import { theme } from 'styles/theme'
+
+const AuthStack = createStackNavigator(
+  {
+    [Routes.Auth]: {
+      screen: AuthScreen,
+      navigationOptions: {
+        header: null,
+      },
+    },
+    [Routes.Signup]: {
+      screen: SignupScreen,
+    },
+  },
+  {
+    initialRouteName: Routes.Auth,
+    defaultNavigationOptions: {
+      headerTintColor: theme.colors.primary,
+      headerStyle: {
+        borderBottomWidth: 0,
+      },
+    },
+  }
+)
+
+export { AuthStack }
