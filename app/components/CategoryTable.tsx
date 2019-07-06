559a2e348bffa3eb6a1a902f4643e199374a4352
Evan Butler <evanb447@gmail.com>
2019-07-05 20:00:16 -0600

    Converts CategoryTable to stateless component

--- app/components/CategoryTable.tsx
+++ app/components/CategoryTable.tsx
@@ -1,6 +1,6 @@
 import { SimpleLineIcons } from '@expo/vector-icons'
 import { GearListItem } from 'components/GearListItem'
-import React, { Component } from 'react'
+import React from 'react'
 import { StyleSheet, View } from 'react-native'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 import { Subheading, Surface, Text } from 'react-native-paper'
@@ -14,73 +14,71 @@ interface Props {
   onRemoveItem: (packItem: PackItem) => void
 }
 
-export class CategoryTable extends Component<Props> {
-  render() {
-    const {
-      category,
-      onAddItems,
-      onDeleteCategory,
-      onPressItem,
-      onRemoveItem,
-    } = this.props
+export function CategoryTable(props: Props) {
+  const {
+    category,
+    onAddItems,
+    onDeleteCategory,
+    onPressItem,
+    onRemoveItem,
+  } = props
 
-    const { name, packItems } = category
+  const { name, packItems } = category
 
-    const totalWeight = () => {
-      if (packItems.length === 0) {
-        return 0
-      }
-      return packItems
-        .map(item => {
-          const weight = Number(item.weight)
-          if (item.units === WeightUnit.GRAMS) {
-            return weight / 453.592
-          }
-          if (item.units === WeightUnit.KILOGRAMS) {
-            return weight * 2.205
-          }
-          if (item.units === WeightUnit.OUNCES) {
-            return weight / 16
-          }
-          return weight
-        })
-        .reduce((prevVal, current) => prevVal + current)
-        .toFixed(2)
+  const totalWeight = () => {
+    if (packItems.length === 0) {
+      return 0
     }
-
-    return (
-      <Surface style={_styles.surface}>
-        <View style={_styles.tableHeaderRow}>
-          <Subheading style={{ fontWeight: 'bold', marginLeft: 2 }}>
-            {name}
-          </Subheading>
-          <TouchableOpacity onPress={() => onAddItems(category)}>
-            <SimpleLineIcons name="plus" size={20} />
-          </TouchableOpacity>
-        </View>
-        {packItems.map((packItem: PackItem) => (
-          <GearListItem
-            key={packItem.uid}
-            gearItem={packItem}
-            onPress={onPressItem}
-            onDelete={onRemoveItem}
-          />
-        ))}
-        <View
-          style={{ flexDirection: 'row', alignItems: 'center', marginTop: 8 }}
-        >
-          <TouchableOpacity onPress={() => onDeleteCategory(name)}>
-            <SimpleLineIcons name="trash" size={20} />
-          </TouchableOpacity>
-          <View style={{ flex: 1, alignItems: 'flex-end' }}>
-            <Text style={{ fontWeight: 'bold', marginRight: 4 }}>
-              {`${totalWeight()} lbs`}
-            </Text>
-          </View>
-        </View>
-      </Surface>
-    )
+    return packItems
+      .map(item => {
+        const weight = Number(item.weight)
+        if (item.units === WeightUnit.GRAMS) {
+          return weight / 453.592
+        }
+        if (item.units === WeightUnit.KILOGRAMS) {
+          return weight * 2.205
+        }
+        if (item.units === WeightUnit.OUNCES) {
+          return weight / 16
+        }
+        return weight
+      })
+      .reduce((prevVal, current) => prevVal + current)
+      .toFixed(2)
   }
+
+  return (
+    <Surface style={_styles.surface}>
+      <View style={_styles.tableHeaderRow}>
+        <Subheading style={{ fontWeight: 'bold', marginLeft: 2 }}>
+          {name}
+        </Subheading>
+        <TouchableOpacity onPress={() => onAddItems(category)}>
+          <SimpleLineIcons name="plus" size={20} />
+        </TouchableOpacity>
+      </View>
+      {packItems.map((packItem: PackItem) => (
+        <GearListItem
+          key={packItem.uid}
+          gearItem={packItem}
+          onPress={onPressItem}
+          onDelete={onRemoveItem}
+        />
+      ))}
+      <View
+        style={{ flexDirection: 'row', alignItems: 'center', marginTop: 8 }}
+      >
+        <TouchableOpacity onPress={() => onDeleteCategory(name)}>
+          <SimpleLineIcons name="trash" size={20} />
+        </TouchableOpacity>
+        <View style={{ flex: 1, alignItems: 'flex-end' }}>
+          <Text style={{ fontWeight: 'bold', marginRight: 4 }}>
+            {`${totalWeight()} lbs`}
+          </Text>
+        </View>
+      </View>
+    </Surface>
+  )
 }
 
 const _styles = StyleSheet.create({
